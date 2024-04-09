24bfe8787fa567ffdb8cf060cd51f3ff898fd35f
Srijita Marick <srijita.marick@ucalgary.ca>
2024-04-09 14:53:56 -0600

    Command line arguments added

--- src/main/java/sea/cpsc233projectgui/LibraryApplication.java
+++ src/main/java/sea/cpsc233projectgui/LibraryApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class LibraryApplication extends Application {
@@ -12,12 +13,36 @@ public class LibraryApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("library.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 600);
+        LibraryController cont = fxmlLoader.getController();
+        cont.load(memberFile, bookFile);
         stage.setTitle("Welcome to Library");
         stage.setScene(scene);
         stage.show();
     }
 
+    private static File bookFile = null;
+
+    private static File memberFile = null;
+
     public static void main(String[] args) {
+
+        if(args.length >2){
+            System.err.println("Expected two command line argument for filename to load from");
+        }
+        if(args.length == 2){
+            String bookFilename = args[0];
+            String memberFilename = args[1];
+            bookFile = new File(args[0]);
+            memberFile = new File(args[1]);
+            if(!bookFile.exists()||!bookFile.canRead()){
+                System.err.println("Cannot load from the file " + bookFilename);
+                System.exit(2);
+            }
+            if(!memberFile.exists()||!memberFile.canRead()){
+                System.err.println("Cannot load from the file " + memberFilename);
+                System.exit(2);
+            }
+        }
         launch();
     }
 }
