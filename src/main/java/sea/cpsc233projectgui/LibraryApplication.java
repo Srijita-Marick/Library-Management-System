d20fcb8256490d967f12731d643605d7ab14ad8b
Anna Littkemann <anna.littkemann@ucalgary.ca>
2024-04-13 22:08:59 -0600

    debug

--- src/main/java/sea/cpsc233projectgui/LibraryApplication.java
+++ src/main/java/sea/cpsc233projectgui/LibraryApplication.java
@@ -18,7 +18,7 @@ public class LibraryApplication extends Application {
         stage.setTitle("Welcome to Library");
         stage.setScene(scene);
         stage.show();
-        //merging error fixing
+        //merging error fixing debug
     }
 
     private static File bookFile = null;
